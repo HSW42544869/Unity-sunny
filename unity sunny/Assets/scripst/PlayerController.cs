be7b76382697036d2848f92be8c1523778d8a99c
HSW42544869 <73380363+HSW42544869@users.noreply.github.com>
2020-11-16 16:32:42 +0800

    sunny(round2)

--- unity sunny/Assets/scripst/PlayerController.cs
+++ unity sunny/Assets/scripst/PlayerController.cs
@@ -3,32 +3,37 @@
 public class PlayerController : MonoBehaviour
 {
     public Rigidbody2D rb;
+    public Animator anim;
     public float speed;
     public float jumpforce;
 
-    private void Start()
+    void Start()
     {
-        
+
     }
 
-    private void FixedUpdate()
+    void FixedUpdate()
     {
         Movement();
     }
 
     void Movement()
     {
-        float horizontalmove =Input.GetAxis("Horizontal");
-        float facedircetion = Input.GetAxisRaw("Horizontion");
+        float horizontalmove = Input.GetAxis("Horizontal");
+        float facedircetion = Input.GetAxisRaw("Horizontal");
 
+        //角色移動
         if (horizontalmove != 0)
-        { 
-        rb.velocity = new Vector2(horizontalmove * Time.deltaTime, rb.velocity.y);
+        {
+            rb.velocity = new Vector2(horizontalmove * speed * Time.deltaTime, rb.velocity.y);
+            anim.SetFloat("running", Mathf.Abs(facedircetion));
         }
+        //角色左右方向畫面
         if (facedircetion != 0)
         {
             transform.localScale = new Vector3(facedircetion, 1, 1);
         }
+        //角色跳躍
         if (Input.GetButtonDown("Jump"))
         {
             rb.velocity = new Vector2(rb.velocity.x, jumpforce * Time.deltaTime);
